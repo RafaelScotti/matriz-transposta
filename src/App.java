8fe359413cc5d2e114a0c9d6059ff8345343e559
rafaelscotti <rafaels.zanella@gmail.com>
2019-08-18 17:24:04 -0300

    make conversion

--- src/App.java
+++ src/App.java
@@ -1,46 +1,38 @@
 import java.io.*; 
 public class App {
-
-
-	
 	public static void main(String[] args) throws IOException{
 		
+		Writer writer = new Writer("matriz_t.txt");
+		Reader reader = new Reader("file.txt");
 
-		
-		char matriz[][] = new char[4][4];
-			
-
-        
+		String matriz[][] = new String[reader.getLinesSize()][reader.getLinesSize()];
 
 		int lin = 0;
 		int col = 0;
-		for(int i = 0; i < everything.length(); i++) {
+		for(int i = 0; i < reader.getSize(); i++) {
 			//System.out.println(i);
-			matriz[lin][col] = everything.charAt(i);
-			//System.out.println(i + ": " + matriz[lin][col]);
+			String x = reader.getContent();
+			matriz[lin][col] = Character.toString(x.charAt(i));
+			System.out.println(matriz[lin][col]);
 			col++;
 			if(col%4==0){
 				lin++;
 				col = 0;
-				//System.out.println("==");
+				System.out.println("==");
 			}
 		}
 
-		// inverter
-		System.out.println("======");
+		System.out.println(reader.getSize());
+		System.out.println(reader.getContent());
+
 		for(int i = 0; i<matriz.length; i++) {
 			for(int j= 0; j < matriz.length; j++ ) {
 				System.out.println(matriz[j][i]); // adicionar no lccol;
+				writer.write(matriz[j][i]);
 			}
-			System.out.println("");
+			writer.write("\n");
 		}
 
-		for(int i = 0; i<matriz.length; i++) {
-			for(int j= 0; j < matriz.length; j++ ) {
-				//System.out.println(matriz[j][i]); // adicionar no lccol;
-				buffWrite.append(matriz[j][i]);
-			}
-			buffWrite.append("\n");
-		}
+
 	}
 }
