518d4594538c2f22e2c6882e310da009fcb4ca9f
rafaelscotti <rafaels.zanella@gmail.com>
2019-08-23 23:39:35 -0300

    refactoring App class

--- src/App.java
+++ src/App.java
@@ -1,46 +1,22 @@
-// import java.io.BufferedWriter;
-// import java.io.FileWriter;
-// import java.io.IOException;
+public class App {
+    public static void main(String[] args) {        
+        
+        String squareMatrix[][] = {
+            {"a", "b", "c"},
+            {"d", "e", "f"},
+            {"g", "h", "i"}
+        };
+        
+        Writer writer = new Writer();
+        Reader reader = new Reader("matriz.txt");
+        Matrix matrix = new Matrix(reader.toMatrix());
 
-// public class App {
-// 	public static void main(String[] args) throws IOException{
-		
-// 		String fileName = "matriz.txt";
-// 		String nextFileName = "matriz_transposta.txt";
-// 		Reader reader = new Reader(fileName);
+        writer.writeIn("matriz_transposta.txt", matrix.transpose());
 
-		
-// 		try{
-// 			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(nextFileName));
-// 			for(int i = 0; i<reader.toMatrix().length; i++) {
-// 				for(int j= 0; j < reader.toMatrix().length; j++ ) {
-// 					buffWrite.append(reader.toMatrix()[j][i]);
-// 				}
-// 				buffWrite.append("\n");
-// 			}
-// 			buffWrite.close();
-			
-//         }catch(IOException e) {
-//             System.out.println(e.getMessage());
-//         }
-	
 
-// 		System.out.println("====Matriz Original====");
-// 		for(int i = 0; i < reader.toMatrix().length; i++){
-// 			for(int j = 0; j < reader.toMatrix().length; j++) {
-// 				System.out.print(reader.toMatrix()[i][j]);
-// 			}
-// 			System.out.println("");
-// 		}
-
-// 		System.out.println("\n\n===Matriz Resultante===");
-// 		for(int i = 0; i < reader.toMatrix().length; i++){
-// 			for(int j = 0; j < reader.toMatrix().length; j++) {
-// 				System.out.print(reader.toMatrix()[j][i]);
-// 			}
-// 			System.out.println("");
-// 		}
-
-	
-// 	}
-// }
+        //matrix.print();
+        matrix.print(matrix.transpose());
+        //matrix.transpose();
+    
+    }
+}
